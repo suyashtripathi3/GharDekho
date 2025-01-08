45bef4bccea92cacbca48cadf880c966a3bde595
Suyash Tripathi <Suyatripathi03@gmail.com>
2025-01-08 21:07:12 +0530

    new

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -2,8 +2,8 @@ import {auth} from 'express-oauth2-jwt-bearer'
 
 
 const jwtCheck = auth({
-    // audience: "http://localhost:5000",
-    audience: "https://ghar-dekho-five.vercel.app/",
+    audience: "http://localhost:5000",
+    // audience: "https://ghar-dekho-five.vercel.app/",
     issuerBaseURL: "https://dev-mi3lvb0lw8sj2zp7.us.auth0.com",
     tokenSigningAlg: "RS256",
 })
