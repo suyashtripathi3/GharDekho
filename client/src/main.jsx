b4b0d6829100d62ee5eeba26cc47a4259a3725c1
Suyash Tripathi <112958155+suyashtripathi3@users.noreply.github.com>
2025-09-13 09:36:18 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           // redirect_uri: "http://localhost:5173/",
           redirect_uri: "https://ghar-dekho-nu.vercel.app",
         }}
-        audience="https://ghar-dekho-fwyq.vercel.app"
+        audience="http://localhost:5000/"
         // audience="https://ghar-dekho-website.vercel.app"
         scope="openid profile email"
         >
