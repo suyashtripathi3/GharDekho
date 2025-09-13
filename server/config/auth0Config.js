6f3cbf963c5d7bc63182a4c1a0022134b17c036c
Suyash Tripathi <112958155+suyashtripathi3@users.noreply.github.com>
2025-09-13 09:26:53 +0530

    Update auth0Config.js

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -2,7 +2,7 @@ import {auth} from 'express-oauth2-jwt-bearer'
 
 
 const jwtCheck = auth({
-    audience: "http://localhost:5000",
+    audience: "https://ghar-dekho-fwyq.vercel.app",
     issuerBaseURL: "https://dev-fmhxba2otfai6a2c.us.auth0.com",
     tokenSigningAlg: "RS256",
 })
